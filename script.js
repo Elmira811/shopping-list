56a5559df57b83f5f7b3d79fef171c2fdc2a3583
Elmira811 <elmira.zyl92@gmail.com>
2025-01-03 15:27:41 +0300

    Update script.js
    Написала script для задания Shopping list

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const input = document.querySelector('#input');
+const itemsContainer = document.querySelector('#items');
+  
+input.addEventListener('keydown', function(event) {
+  const text = input.value;
+
+  const newItem = document.createElement('div');
+  newItem.textContent = text;
+
+  if(event.key == 'Enter' && text != '') {
+    itemsContainer.append(newItem);
+  }
+
+  newItem.addEventListener('click', function() {
+    newItem.classList.toggle('done');
+  })
+
+  input.value = '';
+})
+
+
+  
+  
 /* Новые элементы должны добавляться в список по нажатию на Enter */
 
 /* Пустые элементы не должны добавляться */
